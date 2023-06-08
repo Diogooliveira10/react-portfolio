5a3f693718395c2029df179bbfca5aa65ab3f010
Diogo Oliveira Vieira <ddiogooliveira9@gmail.com>
2023-06-08 19:45:00 -0300

    Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { About } from './components/About'
+import { Contact } from './components/Contact'
+import { Hero } from './components/Hero'
+import { Projects } from './components/Projects'
+import { Services } from './components/Services'
+import { Skills } from './components/Skills'
+import { Testimonials } from './components/Testimonials'
 
 function App() {
   
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <main>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   )
 }
 
---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+export function About() {
+    return (
+        <>
+            <h2 className="text-4x1 underline font-bold">About test</h2>
+        </>
+    )
+}
